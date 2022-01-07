98b0a2d8753c39aac35a36cd766ea4d75d20406f
Potapov Alexey <potapov.alex@hotmail.com>
2022-01-07 22:50:06 +0300

    BasicsSCD adjustments

---
+++ BasicsSCD/Line/Line1.java
@@ -0,0 +1,13 @@
+package com.epam.IntroductionToJavaOnline.BasicsSCD.Line;
+
+//найдите значение функции z=((a-3)*b/2)+c
+
+public class Line1 {
+    static void function(int a, int b, int c) {
+        int z = ((a - 3) * b / 2) + c;
+    }
+
+    public static void main(String[] args) {
+        function(3, 4, 5);
+    }
+}
---
+++ BasicsSCD/Line/Line3.java
@@ -0,0 +1,13 @@
+package com.epam.IntroductionToJavaOnline.BasicsSCD.Line;
+
+//Вычислить значение переменной по формуле(все переменные принимают действительные значения)
+
+public class Line3 {
+    static void arithmeticFormula(int x, int y) {
+        double result = (((Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y))) * (Math.tan((x * y))));
+    }
+
+    public static void main(String[] args) {
+        arithmeticFormula(4, 2);
+    }
+}
